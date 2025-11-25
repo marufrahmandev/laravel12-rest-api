70581b5944d8418bdd974cab51a3de19c5066f27
MarufBD007 <marufrahman08@gmail.com>
2025-11-25 12:00:51 +0600

    Implement api versioning

---
+++ app/Http/Controllers/API/V1/PostController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'message' => 'Posts fetched FROM V1 successfully',
+            'data' => 'Posts FROM V1',
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return response()->json([
+            'message' => 'Post created FROM V1 successfully',
+            'data' => $request->all(),
+        ])->setStatusCode(201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return response()->json([
+            'message' => 'Post fetched FROM V1 successfully',
+            'data' => "Post \"$id\" fetched successfully",
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        return response()->json([
+            'message' => 'Post updated FROM V1 successfully',
+            'data' => $request->all(),
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return response()->json([
+            'message' => 'Post deleted FROM V1 successfully',
+            'data' => "Post \"$id\" deleted successfully",
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,30 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\API\V1\PostController as APIV1PostController;
+use App\Http\Controllers\API\V2\PostController as APIV2PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/hello', function () {
+    return ["message" => "Hello World"];
+});
+
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('posts', APIV1PostController::class);
+});
+
+Route::prefix('v2')->group(function () {
+    Route::apiResource('posts', APIV2PostController::class);
+});
+
+
+
+// Route::apiResource('posts', PostController::class);
+// Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+// Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+// Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
